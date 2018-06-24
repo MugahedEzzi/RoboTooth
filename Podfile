1e5f4cbf1b2126037f4e5bd9ea48a06b8c6bf8a1
Saleem Hadad <saleem_ha@hotmail.com>
2018-06-24 19:03:34 +0800

    add RevealingSplashView library

--- Podfile
+++ Podfile
@@ -5,6 +5,5 @@ target 'RoboTooth' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
   pod "SimpleAnimation"
-  # Pods for RoboTooth
-
+  pod 'RevealingSplashView'
 end
--- Pods/Target Support Files/Pods-RoboTooth/Pods-RoboTooth-frameworks.sh
+++ Pods/Target Support Files/Pods-RoboTooth/Pods-RoboTooth-frameworks.sh
@@ -134,9 +134,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/RevealingSplashView/RevealingSplashView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SimpleAnimation/SimpleAnimation.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/RevealingSplashView/RevealingSplashView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SimpleAnimation/SimpleAnimation.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
